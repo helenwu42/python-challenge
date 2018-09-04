ac183fddde25f378c4854fd93481e16a7680077e
baby <helenwu42@gmail.com>
2018-09-03 19:59:47 -0700

    modify main.py to include greatest increase and decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,8 +8,13 @@ import os
 import csv
 
 #  Lists to store data
-totalMonth = 0     #  number of months
-totalNet = 0       #  The total net amount of "Profit/Losses" over the entire period
+totalMonth = 0             #  number of months
+totalNet = 0               #  The total net amount of "Profit/Losses" over the entire period
+greatestIncreaseM = ""     #  keep track of the greatest increase month
+greatestDecreaseM = ""     #  keep track of the greatest decrease month
+greatestIncrease = 0       #  keep track of the greatest increase amount 
+greatestDecrease = 0       #  keep track of the greatest decrease amount
+
 
 
 # Open the budget_data csv file
@@ -19,15 +24,24 @@ with open(budget_csv, newline="") as csvfile:
     next (csvfile)   #  skip the first header row
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
-        totalMonth = totalMonth + 1
-        totalNet = totalNet + int(row[1])
-
+        totalMonth = totalMonth + 1                # Track total month
+        totalNet = totalNet + int(row[1])          # Track total net amount
+        if greatestIncrease < int(row[1]):
+            greatestIncrease = int(row[1])
+            greatestIncreaseM = row[0]
+        if greatestDecrease > int(row[1]):
+            greatestDecrease = int(row[1])
+            greatestDecreaseM = row[0]             
+         
+        
 
 # print the output in terminal
 print("Financial Analysis")
 print("---------------------------------")
 print("Total Month: " + str(totalMonth))
 print("Total: $" + str(totalNet))
+print("Greatest Increase in Profit: " + greatestIncreaseM + " " + "($" + str(greatestIncrease) + ")")
+print("Greatest Decrease in Profit: " + greatestDecreaseM + " " + "($" + str(greatestDecrease) + ")")
 
 
 #  Open the output file and write to the file
@@ -39,8 +53,9 @@ with open(output_file, "w", newline="") as datafile:
     writer.writerow(["Financial", "Analysis"])
     writer.writerow(["-------------------------------------------"]) 
     writer.writerow(["Total Month:" , str(totalMonth)])
-    writer.writerow(["Total:" , '${}'.format(totalNet)])
-
+    writer.writerow(["Total:" , "$" + str(totalNet)])
+    writer.writerow(["Greatest Increase in Profits: ", greatestIncreaseM, "($" + str(greatestIncrease) + ")"])
+    writer.writerow(["Greatest Decrease in Profits: ", greatestDecreaseM, "($" + str(greatestDecrease) + ")"])
 
 
 
