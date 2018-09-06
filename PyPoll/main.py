b76bf3f5bd2a3fd011ef1761aa77d8e4f3b171bd
baby <helenwu42@gmail.com>
2018-09-06 15:01:08 -0700

    update again

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,6 +14,7 @@ candidate = []             # the candidicates votes
 uniqueC = {}               #  the dictionary holdd the unique candidate and number of votes
 key = []                   #  The unqie candicate in a list
 value = []                 #  The number of votes for each candidate in a list
+percentage = []            #  The percentage of votes for each candidate in a list
 
 # Open the election_data csv file
 
@@ -29,30 +30,28 @@ with open("election_data.csv", newline="") as csvfile:
     #  store the unique candidate and number of votes in a dictionary
     uniqueC = Counter(candidate)
     
+    #  store unique candicate in a list
     for i in uniqueC.keys():
         key.append(i)
     
+    # Store unique votes for each candiates in a list
     for i in uniqueC.values():
         value.append(i)
 
-    # find number of votes and store in a dictionary
-   
-    
+    # Store the percenatge of votes each candicate has in a list
+    for x in range(len(value)):
+        percentage.append(round(((value[x]/sum(value))*100), 3))
 
     #  find the winnervotes
     winnervotes= max(value)
-    print(winnervotes)
-    
-    
-
-    
-        
-    
+  
 # print the output in terminal
 print("Election Results")
 print("---------------------------------")
 print("Total Votes: " + str(len(vote)))
 print("---------------------------------")
+for i in range(0, 4):
+    print(str(key[i]) + ":   " + str(percentage[i]) + "%   " + "(" + str(value[i]) + ")")   
 
 
 
